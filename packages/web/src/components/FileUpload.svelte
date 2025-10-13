ccc41e335eea60f096ab29644f7c73f0e5b4db79
Tom <mocharaia@gmail.com>
2025-10-13 14:58:44 -0400

    fix: restore conditional rendering for supported formats text in simplified mode

--- packages/web/src/components/FileUpload.svelte
+++ packages/web/src/components/FileUpload.svelte
@@ -147,6 +147,8 @@
     <input type="file" {id} {accept} {multiple} on:change disabled={isDisabled} class="file-input" />
 
     <div class="drop-instruction">{isDisabled && !processing ? 'Select a preset in Settings to analyze files' : `or drag and drop ${multiple ? 'files' : 'file'} here`}</div>
-    <div class="file-info">Supported formats: WAV, MP3, FLAC, M4A, OGG</div>
+    {#if !$isSimplifiedMode}
+      <div class="file-info">Supported formats: WAV, MP3, FLAC, M4A, OGG</div>
+    {/if}
   </div>
 </div>
